8bde2452e50e657d7162ac26f4c0b63026b15c66
satvikc <mystic.satvik@gmail.com>
2013-04-03 22:27:14 +0530

    plugin converted to fire custom events

--- experiments/demoPhoneGap/assets/www/js/HeadsetWatcher.js
+++ experiments/demoPhoneGap/assets/www/js/HeadsetWatcher.js
@@ -5,6 +5,18 @@
  * Available under the terms of the MIT License.
  * 
  */
+function HeadsetContext() {
+ 	var hPlugged = document.createEvent('Events');
+	hPlugged.initEvent('headsetplug', true, true);
+	var hUnplugged = document.createEvent('Events');
+	hUnplugged.initEvent('headsetunplug', true, true);	
+	HeadsetWatcher.watch(function(result) {
+    			if(result.plugged) {
+					document.dispatchEvent(hPlugged);
+    			} else {
+      				document.dispatchEvent(hUnplugged);
+    		}})
+}
 var HeadsetWatcher = {
 	watch: function(callback) {
 		return cordova.exec(function(result) {
