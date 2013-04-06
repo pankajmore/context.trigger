e37e1584428d81fe587b25fe165bc70ae219e4de
satvikc <mystic.satvik@gmail.com>
2013-04-06 18:35:28 +0530

    In between moving recipes to file

---
+++ experiments/demoPhoneGap/assets/www/Recipe1.js
@@ -0,0 +1,4 @@
+this.addEventListener('headsetplug',function() {
+    console.log("headset plug event");
+    alert("plugged");
+});
---
+++ experiments/demoPhoneGap/assets/www/js/Timer.js
@@ -0,0 +1,26 @@
+// Accepts a string of date and time when to trigger the event
+// eg. of accepted full string "April 6,2013 10:20:00" 
+function atDate(time,callback){
+    var now = new Date();
+    var at = new Date(time);
+    var millisecs = at - now;
+    if (millisecs > 0) {
+	setTimeout(callback,millisecs);
+    }
+}
+
+// Accepts a string of time in 24hours format like 13:00:00 and
+// triggers the event ever day at that time
+function everyDay(time,callback){
+    var now = new Date();
+    var at = new Date(now.toDateString + " " + time);
+    var millisecs = at - now;
+    if (millisecs <= 0) {
+	millisecs += 86400000;
+	}
+    setTimeout(function() { 
+	callback();
+	everyDay(time,callback);
+	},millisecs);
+}
+    
