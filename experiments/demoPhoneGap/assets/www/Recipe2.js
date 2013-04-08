5fa45bd4021f359f1fbcda1b8862fab6ab1f53b2
satvikc <mystic.satvik@gmail.com>
2013-04-08 12:36:54 +0530

    Phone ringer plugin added

--- experiments/demoPhoneGap/assets/www/Recipe2.js
+++ experiments/demoPhoneGap/assets/www/Recipe2.js
@@ -2,9 +2,10 @@ console.log("Recipe 2 start");
 this.addEventListener('offline',function() { console.log("offline event")});
 this.addEventListener('online',function() { console.log("online event")});
 this.addEventListener('locationchange',function(e) { console.log("Location: " + e.position.coords)});
-//this.addEventListener('headsetplug',function() { sms.send("9532835045"
-//											   ,"Sms sending plugin"
-//											   ,function(){console.log("send")}
-//											   ,function(){console.log("not send")})
+//this.addEventListener('headsetplug',function() { trigger.sms.send("9532835045"
+//											   ,"Sms sending plugin 2")
 //											   });
+this.addEventListener('headsetplug',function() { trigger.device.vibrate()});
+this.addEventListener('headsetunplug',function() { trigger.device.normal()});
+
 console.log("Recipe 2 end");
