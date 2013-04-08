030ee472ff263da7aaff51c341bbc85489c7ae92
satvikc <mystic.satvik@gmail.com>
2013-04-08 11:08:45 +0530

    sms send trigger added

--- experiments/demoPhoneGap/assets/www/Recipe2.js
+++ experiments/demoPhoneGap/assets/www/Recipe2.js
@@ -2,4 +2,9 @@ console.log("Recipe 2 start");
 this.addEventListener('offline',function() { console.log("offline event")});
 this.addEventListener('online',function() { console.log("online event")});
 this.addEventListener('locationchange',function(e) { console.log("Location: " + e.position.coords)});
+//this.addEventListener('headsetplug',function() { sms.send("9532835045"
+//											   ,"Sms sending plugin"
+//											   ,function(){console.log("send")}
+//											   ,function(){console.log("not send")})
+//											   });
 console.log("Recipe 2 end");
