782efe06d001aa051e4a4fc77ccc09cf64f60160
rjdhania <rjdhania@gmail.com>
2013-04-10 05:24:10 -0400

    tts

--- experiments/demoPhoneGap/assets/www/Recipe3.js
+++ experiments/demoPhoneGap/assets/www/Recipe3.js
@@ -1,3 +1,3 @@
 //  Set media volume to half, when earphone is adjusted
 // argument must be from 0 to 15 inclusive.
-this.on('headsetplug',function() {trigger.device.adjust("8")});
+this.on('headsetplug',function() { window.plugins.tts.speak("Earphone Plugged in"); trigger.device.adjust("8")});
